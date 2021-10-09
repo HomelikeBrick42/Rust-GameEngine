f3f662a927a06f420baed8c27a8c12ba6185b6ee
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2021-10-10 09:15:46 +1300

    Opened a Window

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![allow(dead_code)]
 
+use core::ptr::{null, null_mut};
+
 use crate::win32::*;
+use std::process::exit;
+use std::mem::size_of_val;
 
 mod vec3;
 mod win32;
@@ -11,23 +15,80 @@ unsafe extern "system" fn window_message_callback(hWnd: HWND, uMsg: UINT, wParam
 }
 
 fn main() {
-    const WINDOW_CLASS_NAME: LPCSTR = "WindowClassName\0".as_ptr();
-    let instance = unsafe { GetModuleHandleA(std::ptr::null()) };
+    const WINDOW_CLASS_NAME: &str = "WindowClassName\0";
+    const WINDOW_TITLE: &str = "Test Window\0";
+    const WINDOW_STYLE: DWORD = WS_OVERLAPPED | WS_SYSMENU | WS_MINIMIZEBOX | WS_CAPTION;
+    const WINDOW_STYLE_EX: DWORD = 0;
+    const WINDOW_WIDTH: u32 = 640;
+    const WINDOW_HEIGHT: u32 = 480;
+
+    let instance = unsafe { GetModuleHandleA(null()) };
 
     unsafe {
         let mut window_class = WNDCLASSEXA::default();
 
+        window_class.cbSize = size_of_val(&window_class) as UINT;
         window_class.hInstance = instance;
-        window_class.lpfnWndProc = Some(window_message_callback);
-        window_class.lpszClassName = WINDOW_CLASS_NAME;
+        window_class.lpfnWndProc = Some(DefWindowProcA);
+        window_class.lpszClassName = WINDOW_CLASS_NAME.as_ptr();
+        // TODO: window_class.hCursor = LoadCursorA(null_mut(), IDC_ARROW);
 
         let result = RegisterClassExA(&window_class);
-        if result != 0 {
-            eprintln!("Failed to register window class with code: {}", result);
+        if result == 0 {
+            eprintln!("Failed to register window class with code: {}", GetLastError());
+            exit(1);
+        }
+    }
+
+    let window = unsafe {
+        let mut rect = RECT::default();
+        rect.left = 100;
+        rect.right = rect.left + WINDOW_WIDTH as i32;
+        rect.top = 100;
+        rect.bottom = rect.top + WINDOW_HEIGHT as i32;
+
+        if AdjustWindowRectEx(&mut rect, WINDOW_STYLE, 0, WINDOW_STYLE_EX) == 0 {
+            eprintln!("Failed to calculate window width and height with code: {}", GetLastError());
+            exit(1);
+        }
+
+        let width = rect.right - rect.left;
+        let height = rect.bottom - rect.top;
+
+        CreateWindowExA(
+            WINDOW_STYLE_EX,
+            WINDOW_CLASS_NAME.as_ptr(),
+            WINDOW_TITLE.as_ptr(),
+            WINDOW_STYLE,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            width,
+            height,
+            null_mut(),
+            null_mut(),
+            instance,
+            null_mut(),
+        )
+    };
+    if window.is_null() {
+        eprintln!("Failed to create window with code: {}", unsafe { GetLastError() });
+        exit(1);
+    }
+
+    unsafe { ShowWindow(window, SW_SHOW) };
+
+    loop {
+        unsafe {
+            let mut message = MSG::default();
+            while PeekMessageA(&mut message, window, 0, 0, PM_REMOVE) != 0 {
+                TranslateMessage(&message);
+                DispatchMessageA(&message);
+            }
         }
     }
 
     unsafe {
-        UnregisterClassA(WINDOW_CLASS_NAME, instance);
+        DestroyWindow(window);
+        UnregisterClassA(WINDOW_CLASS_NAME.as_ptr(), instance);
     }
 }
--- src/win32.rs
+++ src/win32.rs
@@ -1,26 +1,50 @@
+pub use crate::win32::internal::*;
+
 #[allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
 mod internal {
-    use std::os::raw::{c_int, c_uint};
+    use std::os::raw::c_int;
 
     pub type VOID = std::ffi::c_void;
-    pub type UINT = c_uint;
+    pub type LPVOID = *mut VOID;
+    pub type UINT = u32;
     pub type HANDLE = *mut VOID;
     pub type UINT_PTR = *mut UINT;
     pub type LONG_PTR = isize;
     pub type LPCSTR = *const u8;
     pub type WORD = u16;
-    pub type BOOL = c_int;
+    pub type BOOL = i32;
+    pub type DWORD = u32;
+    pub type LONG = i32;
 
     pub type HICON = HANDLE;
     pub type HINSTANCE = HANDLE;
     pub type HCURSOR = HICON;
     pub type HBRUSH = HANDLE;
     pub type HMODULE = HANDLE;
+    pub type HMENU = HANDLE;
     pub type HWND = HANDLE;
     pub type WPARAM = UINT_PTR;
     pub type LPARAM = LONG_PTR;
     pub type LRESULT = LONG_PTR;
     pub type ATOM = WORD;
+
+    pub const WS_OVERLAPPED: u32 = 0x00000000;
+    pub const WS_CAPTION: u32 = 0x00C00000;
+    pub const WS_SYSMENU: u32 = 0x00080000;
+    pub const WS_THICKFRAME: u32 = 0x00040000;
+    pub const WS_MINIMIZEBOX: u32 = 0x00020000;
+    pub const WS_MAXIMIZEBOX: u32 = 0x00010000;
+    pub const WS_OVERLAPPEDWINDOW: u32 = WS_OVERLAPPED
+        | WS_CAPTION
+        | WS_SYSMENU
+        | WS_THICKFRAME
+        | WS_MINIMIZEBOX
+        | WS_MAXIMIZEBOX;
+    pub const CW_USEDEFAULT: c_int = 0x80000000_u32 as c_int;
+
+    pub const SW_SHOW: c_int = 5;
+
+    pub const PM_REMOVE: UINT = 0x0001;
 
     pub type WNDPROC = Option<
         unsafe extern "system" fn(
@@ -44,6 +68,17 @@ mod internal {
     }
 
     #[repr(C)]
+    pub struct RECT {
+        pub left: LONG,
+        pub right: LONG,
+        pub top: LONG,
+        pub bottom: LONG,
+    }
+    unsafe_impl_default_zeroed!(RECT);
+
+    pub type LPRECT = *mut RECT;
+
+    #[repr(C)]
     pub struct WNDCLASSEXA {
         pub cbSize: UINT,
         pub style: UINT,
@@ -60,16 +95,58 @@ mod internal {
     }
     unsafe_impl_default_zeroed!(WNDCLASSEXA);
 
+    #[repr(C)]
+    pub struct POINT {
+        x: LONG,
+        y: LONG,
+    }
+    unsafe_impl_default_zeroed!(POINT);
+
+    #[repr(C)]
+    pub struct MSG {
+        hwnd: HWND,
+        message: UINT,
+        wParam: WPARAM,
+        lParam: LPARAM,
+        time: DWORD,
+        pt: POINT,
+        lPrivate: DWORD,
+    }
+    unsafe_impl_default_zeroed!(MSG);
+
+    pub type LPMSG = *mut MSG;
+
     #[link(name = "Kernel32")]
     extern "system" {
         pub fn GetModuleHandleA(lpModuleName: LPCSTR) -> HMODULE;
+        pub fn GetLastError() -> DWORD;
     }
 
     #[link(name = "User32")]
     extern "system" {
         pub fn RegisterClassExA(lpWndClass: *const WNDCLASSEXA) -> ATOM;
         pub fn UnregisterClassA(lpClassName: LPCSTR, hInstance: HINSTANCE) -> BOOL;
+        pub fn CreateWindowExA(
+            dwExStyle: DWORD,
+            lpClassName: LPCSTR,
+            lpWindowName: LPCSTR,
+            dwStyle: DWORD,
+            X: c_int,
+            Y: c_int,
+            nWidth: c_int,
+            nHeight: c_int,
+            hWndParent: HWND,
+            hMenu: HMENU,
+            hInstance: HINSTANCE,
+            lpParam: LPVOID,
+        ) -> HWND;
+        pub fn DestroyWindow(hWnd: HWND) -> BOOL;
+        pub fn DefWindowProcA(hWnd: HWND, Msg: UINT, wParam: WPARAM, lParam: LPARAM) -> LRESULT;
+        pub fn ShowWindow(hWnd: HWND, nCmdShow: c_int) -> BOOL;
+        pub fn AdjustWindowRectEx(lpRect: LPRECT, dwStyle: DWORD, bMenu: BOOL, dwExStyle: DWORD) -> BOOL;
+        pub fn PeekMessageA(lpMsg: LPMSG, hWnd: HWND, wMsgFilterMin: UINT, wMsgFilterMax: UINT, wRemoveMsg: UINT) -> BOOL;
+        pub fn TranslateMessage(lpMsg: *const MSG) -> BOOL;
+        pub fn DispatchMessageA(lpMsg: *const MSG) -> LRESULT;
+        pub fn LoadCursorA(hInstance: HINSTANCE, lpCursorName: LPCSTR) -> HCURSOR;
     }
 }
-
-pub use crate::win32::internal::*;
